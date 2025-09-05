82b1fe8693dc9364db2ce912f630fbcb8d330f0d
charlie <charlesmwangi1008@gmail.com>
2025-09-05 12:32:25 +0300

    [solution:] converting a string to a number

---
+++ ch-01_values_types_and_operators/7-conversion.js
@@ -0,0 +1,14 @@
+let value = "79";
+let value2 = 97;
+let result;
+let result2;
+result2 = value + value2;
+console.log(result2);
+console.log(typeof result2);
+
+value = Number(value);
+console.log(value);
+
+result = value + value2;
+console.log(result);
+console.log(typeof result);
