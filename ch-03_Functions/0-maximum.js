10236784ca8aaf9c21aa8d1f6868260ef8d311b3
charlie <charlesmwangi1008@gmail.com>
2025-10-06 09:20:55 +0300

    [solution:]

---
+++ ch-03_Functions/0-maximum.js
@@ -0,0 +1,11 @@
+// function definition 
+function max(a, b) {
+    if(a > b) {
+        return a;
+    }else if (b > a) {
+        return b;
+    }
+}
+// function call
+console.log(max(16, 10));
+console.log(max(30, -1500));
