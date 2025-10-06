d105d300d47ad9919f950b87dcf1f4531ecd9f51
charlie <charlesmwangi1008@gmail.com>
2025-10-06 10:25:09 +0300

    [solution:] absolute function using arrow function notation

---
+++ ch-03_Functions/6-absolute.js
@@ -0,0 +1,15 @@
+// arrow funtion definition
+
+let abs = (x) => {
+
+    if(x >= 0) {
+        return x;
+
+    }else if (x < 0) {
+        return x = x / -1;
+    } 
+}
+// fucntion call
+console.log(abs(10));
+console.log(abs(-25));
+console.log(abs(0));
