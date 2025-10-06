913b5d8cff96b9f67b4c10d385e5230cf7f0201f
charlie <charlesmwangi1008@gmail.com>
2025-10-06 09:27:15 +0300

    [solution:] arrow function notation for maximum function

---
+++ ch-03_Functions/1-maximum.js
@@ -0,0 +1,11 @@
+// arrow function defintion 
+let max = (a, b) => {
+    if(a > b) {
+        return a;
+    }else{
+        return b;
+    }
+}
+// function call
+console.log(max(30, 15));
+console.log(max(25, 75));
