1a4f2f4c987bd6864aa60c5e4d7c866474b4f6f6
chuckycheese09 <charlesmwangi1008@gmail.com>
2026-02-21 11:52:39 +0300

    [solution:] arrow function firstElement that takes an array as a parameter and retruns the first element in an array

---
+++ ch-04_DataStructures/1-firstElement.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ * @param {[]} array - the array for which we are getting the first element
+ * @returns {} first element in an array
+ */
+let firstElement = (array) => {
+    return array[0];
+}
+console.log(firstElement([10, 20, 30, 40]));
+
